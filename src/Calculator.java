cadd6d0ee8396b756f47add898ac1cb7a40b1b7c
MedMath <medmathandit@gmail.com>
2019-01-23 13:48:34 -0500

    added operation5

--- src/Calculator.java
+++ src/Calculator.java
@@ -35,7 +35,9 @@ public static int operation1(int num1, int num2) {
    public static double operation4(int num1, int num2){
       return num1*num2+num2;
    }
-
+   public static double operation6(int num1, int num2){
+      return num1/num2+num1;
+   }
     public static double operation5(int num1, int num2){
         return num1/num2-num1;
     }
