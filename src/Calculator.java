7ff40aaf385a59dc815c85ba302240557fbf0524
medmath <46496303+medmath@users.noreply.github.com>
2019-01-23 13:10:26 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -28,5 +28,10 @@ public static int operation1(int num1, int num2) {
     public static int operation2(int num1, int num2){
         return Math.abs(num1)+Math.max(num1, num2);
     }
+   
+   public static double operation3(int num1, int num2){
+      return num1*num2-num1;
+   }
+   
 }
 //https://github.com/CybertekSchool/parallel-multi-browser-testng-framework.git
