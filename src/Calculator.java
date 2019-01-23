7f61d585ac8ec847d355a1b05fe3c0424f3891f9
medmath <46496303+medmath@users.noreply.github.com>
2019-01-23 13:19:01 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -35,5 +35,8 @@ public static int operation1(int num1, int num2) {
    public static double operation4(int num1, int num2){
       return num1*num2+num2;
    }
+   public static double operation6(int num1, int num2){
+      return num1/num2+num1;
+   }
 }
 //https://github.com/CybertekSchool/parallel-multi-browser-testng-framework.git
